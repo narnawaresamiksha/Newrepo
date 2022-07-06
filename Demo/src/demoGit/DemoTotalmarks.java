795ead5c3fbb95597bc386a352d4b20ad6816563
narnawaresamiksha <snarnaware97@gmail.com>
2022-07-06 14:53:59 +0530

    first pg

---
+++ Demo/src/demoGit/DemoTotalmarks.java
@@ -0,0 +1,21 @@
+package demoGit;
+
+public class DemoTotalmarks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Sub1,Sub2,Sub3,Total,Avg;
+		Sub1=98;
+		Sub2=68;
+		Sub3=55;
+		
+		Total= Sub1+Sub2+Sub3;
+		Avg=Sub1+Sub2+Sub3/3;
+		System.out.println("Total="+Total);
+		System.out.println("Avg="+Avg);
+		
+
+
+	}
+
+}
